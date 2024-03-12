77f3438de1d6439bd248a094e691400e7ae7290b
singh-prabhjot2005 <115982155+singh-prabhjot2005@users.noreply.github.com>
2024-03-12 12:25:21 +0100

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -14,5 +14,25 @@
         $saved_link = json_decode($_cookie[$cookie_content], true);
         echo "<h2>Link salvati:</h2>";
         echo "<ul>";
+        foreach ($saved_link as $link){
+            echo "<li><a href='$link'>$link</a></li>";
     }
+    echo "</ul>";
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Link saver</title>
+</head>
+<body>
+    <h1>save your link:</h1>
+    <form method="post">
+    <label for="link">Inserisci il tuo link:</label><br>
+    <input type="text" id="link" name="link" required><br>
+    <input type="submit" value="save">
+</form>
+</body>
+</html>
