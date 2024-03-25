826d032692111bf444819deac35c79c49c3b3390
singh-prabhjot2005 <115982155+singh-prabhjot2005@users.noreply.github.com>
2024-03-25 09:19:12 +0100

    Update restricted.php

--- restricted.php
+++ restricted.php
@@ -6,7 +6,9 @@
     echo '<button onclick="location.href=\'login.php\'">logout '.
     $_SESSION['email'] . '</button><br>';
 
-
+    if($lastEmail) {
+    echo 'Ultima email utilizzata per l\'accesso: ' . $lastEmail;
+}
 ?>
 
 <h1>PAGINA RISERVATA</h1>
